375430cc7763abebdfd4c94a728717bc7bae95ed
Vishnu-Nadh <vishnunadh222@gmail.com>
2022-11-30 21:31:57 +0530

    page not found design added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,11 +6,14 @@ const NotFound = () => {
   return (
     <div className={styles.notfound}>
       <div className={styles.notfound__content}>
-        <h2>404</h2>
-        <h3>Oops ! Page Not Found</h3>
+        <div class={styles.content}>
+          <h2 class={styles.text_shadows}>404</h2>
+        </div>
+        <h2></h2>
+        <h3 className="heading-primary">Oops! Page Not Found.</h3>
         <p>This page does not exist! we suggest you back to home</p>
         <Link to="/">
-          <button>Back To Home</button>
+          <button className="btn-primary">Back To Home</button>
         </Link>
       </div>
     </div>
